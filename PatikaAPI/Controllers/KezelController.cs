59c2f8a3a1d8fbeb04549167b000830c47f104ac
Laci <1sikelaszlo@gmail.com>
2024-12-06 11:06:50 +0100

    JsonIgnore ehelyezve

---
+++ PatikaAPI/Controllers/KezelController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PatikaAPI.Models;
+
+namespace PatikaAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class KezelController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            using (var context = new PatikaContext())
+            {
+                try
+                {
+                    List<Kezel> kezelesek = context.Kezels.ToList();
+                    return Ok(kezelesek);
+                }
+                catch (Exception ex)
+                {
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
