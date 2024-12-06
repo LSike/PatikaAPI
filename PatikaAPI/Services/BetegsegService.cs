ac674876e1ffd255efad8beb56e45ad7fbbbb4ae
Laci <1sikelaszlo@gmail.com>
2024-12-06 07:54:07 +0100

    Add project files.

---
+++ PatikaAPI/Services/BetegsegService.cs
@@ -0,0 +1,58 @@
+﻿using PatikaAPI.Models;
+
+namespace PatikaAPI.Services
+{
+    public class BetegsegService
+    {
+        public static string Delete(int id)
+        {
+            using (var context = new PatikaContext())
+            {
+                
+                try
+                {
+                    Betegseg torlendo = context.Betegsegs.FirstOrDefault(b => b.Id == id);
+                    if (torlendo is not null)
+                    {
+                        context.Betegsegs.Remove(torlendo);
+                        context.SaveChanges();
+                        return "Sikeres törlés!";
+                    }
+                    else
+                    {
+                        return "Nincs ilyen azonosítójú betegség";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    return $"{ex.Message}";
+                }
+            }
+        }
+
+        public async static void Post(Betegseg betegseg)
+        {
+            using (var context = new PatikaContext())
+            {
+                try
+                {
+                    if (betegseg is not null)
+                    {
+                        await context.Betegsegs.AddAsync(betegseg);
+                        await context.SaveChangesAsync();
+                        //return "Sikeres felvitel.";
+                    }
+                    else
+                    {
+                        //return "Üres adatokat kaptam!";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    //return $"Hiba! {ex.Message}";
+                    
+                }
+            }
+        }
+    }
+}
